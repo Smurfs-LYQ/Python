cef16e68aa44e578f3bcb8095c948bd74ca56bfa
Smurfs的格格巫 <liyueqiang_it@163.com>
2019-03-21 20:20:24 +0800

    new

---
+++ day1/one.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+'''
+# 打印1,2,3,4,5,6,8,9,10
+one = 1
+while one <= 10 :
+    if one == 7 :
+        one += 1
+        continue
+    print(one)
+    one += 1
+'''
+
+'''
+# 打印100以内所有的奇数
+# 方法1
+one = 1
+while one <= 100 :
+    if one % 2 :
+        print(one)
+    one += 1
+
+# 方法2
+one = 1
+while one <= 100 :
+    print(one)
+    one += 2
+'''
--- test/one.py
+++
@@ -1,7 +0,0 @@
-one = 1
-two = 0
-while one <= 10 :
-    two += one
-    one += 1
-
-print(two)
